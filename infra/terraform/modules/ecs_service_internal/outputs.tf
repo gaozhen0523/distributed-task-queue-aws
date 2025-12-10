16d9991921fbbf40a8e986064d7dec50ac9286bf
gaozhen0523 <gaozhen0523@gmail.com>
2025-12-09 21:05:05 -0600

    Day 19： fix bug

--- infra/terraform/modules/ecs_service_internal/outputs.tf
+++ infra/terraform/modules/ecs_service_internal/outputs.tf
@@ -1,8 +1,25 @@
 #infra/terraform/modules/ecs_service_internal/outputs.tf
 output "service_name" {
-  value = aws_ecs_service.this.name
+  description = "Name of the ECS service"
+  value       = aws_ecs_service.this.name
 }
 
 output "task_definition_arn" {
-  value = aws_ecs_task_definition.this.arn
+  description = "ARN of the ECS task definition"
+  value       = aws_ecs_task_definition.this.arn
+}
+
+output "task_role_arn" {
+  description = "IAM role ARN used by ECS tasks"
+  value       = aws_iam_role.task.arn
+}
+
+output "task_execution_role_arn" {
+  description = "IAM execution role ARN used by ECS tasks"
+  value       = aws_iam_role.execution.arn
+}
+
+output "environment_variables" {
+  description = "Environment variables for the container"
+  value       = var.environment_variables
 }
